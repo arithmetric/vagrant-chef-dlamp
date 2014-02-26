4723867911cbc63e8d366af503354a77be746d02
Joe Turgeon <jturgeon@phase2technology.com>
2014-02-26 13:53:19 -0600

    Updating drupal_demo and drupal_test cookbooks to only set up Drupal demo site and testing tools if not already run.

--- cookbooks/bdd-cookbooks/drupal_demo/recipes/default.rb
+++ cookbooks/bdd-cookbooks/drupal_demo/recipes/default.rb
@@ -2,9 +2,11 @@
 drush_execute "pm-download" do
   cwd "/vagrant"
   options %W{ drupal --drupal-project-rename="docroot" }
+  not_if { File.exist?("/vagrant/docroot/index.php") }
 end
 
 drush_execute "site-install" do
   cwd "/vagrant/docroot"
   options %W{ --account-name=admin --account-pass=admin --db-su=root --db-su-pw=root --db-url=mysql://drupaldemo:drupaldemo@localhost/drupaldemo }
+  not_if { File.exist?("/vagrant/docroot/sites/default/settings.php") }
 end
